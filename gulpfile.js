e9e8aabd670a82799ea9e6cece1004d1be048307
mrzhang90 <sp6688@vip.qq.com>
2019-08-07 16:14:48 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const react = require('gulp-react');
-const babel = require('gulp-babel');
 const watch = require('gulp-watch');
-const assetRev = require('gulp-asset-rev');
+const useref = require('gulp-useref')//进行资源合并
+const assetRev = require('gulp-asset-rev');//添加版本号
+const rev = require('gulp-rev')//后缀
+const revCollector = require('gulp-rev-collector')//根据rev生成的manifest.json文件中的映射, 替换文件名称\路径
+const gutil = require('gulp-util');//打印日志
+// js
+const jshint = require('gulp-jshint');//jshint
+const uglify = require('gulp-uglify');//js压缩
+const babel = require('gulp-babel');
+const postcss = require('gulp-postcss');
 const csso = require('gulp-csso');
-const uglify = require('gulp-uglify');
 const browserify = require('browserify');
 const shim = require('browserify-shim');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const filter = require('gulp-filter');
-const rev = require('gulp-rev')
-const revCollector = require('gulp-rev-collector')
 const del = require('del')
-const jsFilter = filter('./html/static/js/pdfJs/',{restore:true}),
+const jsFilter = filter(['./html/**/*.js'],{restore:true}),
         cssFilter = filter('./html/**/*.css',{restore:true}),
         htmlFilter = filter(['./html/**/*.html'],{restore:true});
 //css文件压缩，更改版本号，并通过rev.manifest将对应的版本号用json表示出来
@@ -24,12 +27,22 @@ gulp.task('build_css', () => {
 	}, () => {
         gulp.src('./html/**/*.css')
         .pipe(csso())
-        // .pipe(assetRev())
-        .pipe(rev())
-        .pipe(gulp.dest('./dist/'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('./dist/rev/css'))
+        .pipe(assetRev())
+        .pipe(rev())//添加hash后缀
+        .pipe(gulp.dest('./dist/'))//移动到dist/css
+        .pipe(rev.manifest())//生成文件映射
+        .pipe(gulp.dest('./dist/rev/css'))//将映射文件导出到rev/css
     });
+}); 
+//jshint
+gulp.task('jshint', () => {
+    // return watch('./html/**/*.js', {
+    //     ignoreInitial: false //指示chokidar是否应该忽略初始添加事件
+    // }, () => {
+    //     gulp.src('./html/**/*.js')
+    //     .pipe(jshint())
+    //     .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
+    // });
 });
 //js文件压缩，更改版本号，并通过rev.manifest将对应的版本号用json表示出
 gulp.task('build_js', () => {
@@ -38,8 +51,15 @@ gulp.task('build_js', () => {
 	}, () => {
         gulp.src('./html/**/*.js')
         // .pipe(jsFilter)
-        // .pipe(uglify())
-        // .pipe(assetRev())
+        // .pipe(uglify({
+        //     mangle: true,               // 是否修改变量名，默认为 true
+        //     compress: true,             // 是否完全压缩，默认为 true
+        //     preserveComments: 'all'     // 保留所有注释
+        // }))
+        //  .on('error', function (err) {
+        //     gutil.log(gutil.colors.red('[Error]'), err.toString());
+        // })
+        .pipe(assetRev())
         // .pipe(jsFilter.restore)
         .pipe(rev())
         .pipe(gulp.dest('./dist/'))
@@ -60,27 +80,15 @@ gulp.task('build_img', () => {
     });
 });
 gulp.task('build_file', () => {
-    return watch('./html/**/*.pdf', {
-		ignoreInitial: false //指示chokidar是否应该忽略初始添加事件
-	}, () => {
-        gulp.src('./html/**/*.pdf')
-        .pipe(gulp.dest('./dist/'));
-    });
+    gulp.src('./html/**/*.{pdf,swf,xap,bcmap,svg,ttf,woff}')
+    // gulp.src([
+    //     './html/**/*.pdf','./html/**/*.swf',
+    //     './html/**/*.xap','./html/**/*.bcmap',
+    //     './html/**/*.svg','./html/**/*.ttf',
+    //     './html/**/*.woff'
+    // ])
+    .pipe(gulp.dest('./dist/'));
 });
-// gulp.task('build_component', () => {
-//     return watch('./html/component/*.js', {
-// 		ignoreInitial: false //指示chokidar是否应该忽略初始添加事件
-// 	}, () => {
-//         browserify('./html/component/main.js')
-//         .transform(babelify, {
-//             presets: ['es2015', 'react']
-//         })
-//         .transform(shim)
-//         .bundle()
-//         .pipe(source('main.js'))
-//         .pipe(gulp.dest('../apache-tomcat-8.5.30/webapps/ROOT/assets/script'));
-//     });
-// });
 //通过hash来精确定位到html模板中需要更改的部分,然后将修改成功的文件生成到指定目录
 gulp.task('rev',function(){
     return gulp.src(['dist/rev/**/*.json','./html/**/*.html'])
@@ -115,4 +123,4 @@ gulp.task('del',function () {
     del('./dist/');                               // 构建前先删除dist文件里的旧版本
 })
 // gulp.task('default', ['del',process.env.NODE_ENV == "production" ? 'buildprod' : 'builddev1','builddev2','builddev3','builddev4']);
-gulp.task('default', [process.env.NODE_ENV == "production" ? 'buildprod' : 'build_css','build_js','build_img','build_file','rev','build_html']);
+gulp.task('default', ['build_css','jshint','build_js','build_img','build_file','rev','build_html']);
